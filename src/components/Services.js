bd3dc6d23e02a71753bd9ab8b2aa9b4ee0a24802
vinaypump <vinaykennedy@gmail.com>
2022-04-29 13:33:50 +0530

    styled services section

--- src/components/Services.js
+++ src/components/Services.js
@@ -8,10 +8,8 @@ const Services = () => {
       allWpServices {
         edges {
           node {
-            featuredImage {
-              node {
-                sourceUrl
-              }
+            serviceType {
+              servicetype
             }
             title
           }
@@ -40,10 +38,21 @@ const Services = () => {
           <h2>{servicesTitle}</h2>
           <p>{serviceDescription}</p>
         </Topcontents>
-        {servicesList.map((serviceList, index) => {
-          console.log(serviceList)
-          return <></>
-        })}
+        <div className="services-listings">
+          {servicesList.map((serviceList, index) => {
+            console.log()
+            return (
+              <ServicesItem key={index}>
+                <figure
+                  dangerouslySetInnerHTML={{
+                    __html: serviceList.node.serviceType["servicetype"],
+                  }}
+                ></figure>
+                <p>{serviceList.node.title}</p>
+              </ServicesItem>
+            )
+          })}
+        </div>
       </Wrapper>
     </ServicesSec>
   )
@@ -51,6 +60,47 @@ const Services = () => {
 
 const ServicesSec = styled.div`
   padding: 5em 0;
+  .services-listings {
+    display: grid;
+    grid-gap: 2em;
+    grid-template-columns: repeat(auto-fill, calc(94% / 3));
+    margin: 3em 0 0;
+  }
 `
-
+const ServicesItem = styled.div`
+  display: flex;
+  padding: 1.2em 1.7em;
+  background: #111111;
+  box-shadow: 0px 15px 20px #00000014;
+  transition: 0.3s all ease;
+  align-items: center;
+  p {
+    flex-basis: 80%;
+  }
+  figure {
+    flex-basis: 3.71em;
+    height: 3.5em;
+    background: #000000;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 1em 0 0;
+    svg {
+      max-width: 50%;
+    }
+  }
+  &:hover {
+    background: #ff4900;
+    border-radius: 0.3em;
+    figure {
+      background-color: #fff;
+    }
+    svg {
+      path {
+        fill: #000;
+      }
+    }
+  }
+`
 export default Services
--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,26 +1,15 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
+import { Wrapper, Topcontents } from "../styles/baseStyles"
 
 const ShowCase = () => {
-  const showCaseDatas = useStaticQuery(graphql`
-    query showCaseQuery {
-      allWpPage {
-        edges {
-          node {
-            showcaseItem {
-              showcaseGroups {
-                awards
-                clients
-                fieldGroupName
-                projects
-                yearsExperience
-              }
-            }
-          }
-        }
-      }
-    }
-  `)
+  // const showCaseDatas = useStaticQuery(graphql`
+  //   query showCaseQuery {
+
+  //   }
+  // `)
+  console.log(showCaseDatas)
   return (
     <div>
       <h1>ShowCase</h1>
